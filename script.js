f79309f8c631f9a6b82f8a4d0daea935b60208e6
bikash2911 <raibikas2911@gmail.com>
2025-04-28 06:26:59 +0300

    resolved all ui/ux and fix responsive

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let btn1 = document.querySelector("#btn1");
 let btn2 = document.querySelector("#btn2");
-
 let copyCode = document.querySelector(".code-div");
+const openBtn = document.getElementById("openPopup");
+const popup = document.getElementById("popup");
 
 let rgb1 = "#00f";
 let rgba2 = "#fff";
@@ -17,19 +18,35 @@ const hexValues = function () {
 
 const handlebtn1 = () => {
   rgb1 = hexValues();
-  document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgba2})`;
-  copyCode.innerHTML = `background-image: linear-gradient(to right,${rgb1}, ${rgba2})`;
+  document.body.style.backgroundImage = `linear-gradient(to right, ${rgba2}, ${rgb1})`;
+  copyCode.innerHTML = `background-image: linear-gradient(to right,${rgba2}, ${rgb1})`;
 };
 
 const handlebtn2 = () => {
   rgba2 = hexValues();
-  document.body.style.backgroundImage = `linear-gradient(to right, ${rgba2}, ${rgb1})`;
-  copyCode.innerHTML = `background-image: linear-gradient(to right,${rgba2}, ${rgb1})`;
+  document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgba2})`;
+  copyCode.innerHTML = `background-image: linear-gradient(to right,${rgb1}, ${rgba2})`;
 };
 
 btn1.addEventListener("click", handlebtn1);
 btn2.addEventListener("click", handlebtn2);
 
 copyCode.addEventListener("click", () => {
-    navigator.clipboard.writeText(copyCode.innerText);
+  navigator.clipboard.writeText(copyCode.innerText);
+});
+// click paxi hover effect dina ko lagi
+btn2.addEventListener("click", () => {
+  btn2.classList.add("hover-enabled");
+  setTimeout(() => {
+    btn2.classList.remove("hover-enabled");
+  }, 300);
+});
+
+// copy pupup btn
+
+openBtn.addEventListener("click", function () {
+  popup.style.display = "block";
+  setTimeout(() => {
+    popup.style.display = "none";
+  }, 1000);
 });
